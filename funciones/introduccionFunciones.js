f25b3448119f85e228ff58bce4ad370e30a8602d
Marco Contreras <mcontreras42@outlook.com>
2025-03-22 22:25:19 -0300

    Subiendo ejercicio de introduccion de funciones

--- funciones/introduccionFunciones.js
+++ funciones/introduccionFunciones.js
@@ -7,4 +7,16 @@ Palabras clave: definir, llamar.
 Las funciones son bloques de código que nos permiten reutilizar y estructurar nuestro código de una manera más organizada. Al definir una función, 
 estamos creando un conjunto de instrucciones que realizan una tarea específica. Luego, podemos llamar a esa función en cualquier parte de nuestro 
 programa cada vez que necesitemos ejecutar esa tarea. Esto nos ayuda a evitar la repetición de código en nuestras aplicaciones.
+
+Ejercicio
+Se tiene definida la función "despedir", pero no se está llamando. Llama a la función despedir para que muestre "Adiós Mundo".
 */
+
+function despedir() {
+    console.log("Adiós Mundo");
+  }
+  /* Escribe tu código aquí */
+  
+  despedir(); // Llamando a la función despedir y es la respuesta al ejercicio.
+  
+  /* Fin */
