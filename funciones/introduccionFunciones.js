9a61901080b83b08ba16cf7f8d9c054eae30c67d
Marco Contreras <mcontreras42@outlook.com>
2025-03-22 22:15:02 -0300

    subiendo introduccion a las funciones

---
+++ funciones/introduccionFunciones.js
@@ -0,0 +1,10 @@
+/*Introducción a funciones
+Idea clave
+Las funciones nos permiten reutilizar código.
+Las funciones deben ser definidas una vez y luego pueden ser llamadas a lo largo del programa.
+Palabras clave: definir, llamar.
+¿Qué son las funciones?
+Las funciones son bloques de código que nos permiten reutilizar y estructurar nuestro código de una manera más organizada. Al definir una función, 
+estamos creando un conjunto de instrucciones que realizan una tarea específica. Luego, podemos llamar a esa función en cualquier parte de nuestro 
+programa cada vez que necesitemos ejecutar esa tarea. Esto nos ayuda a evitar la repetición de código en nuestras aplicaciones.
+*/
