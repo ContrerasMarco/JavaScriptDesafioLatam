927f0fa5e542143ea084ffa24bd3043e613aceb8
Marco Contreras <mcontreras42@outlook.com>
2025-04-08 19:15:10 -0400

    Subiendo ejercicios de if ternario

---
+++ profundizandoFunciones/15-IfTernario.js
@@ -0,0 +1,22 @@
+/*El if ternario es una forma de escribir un if en una sola línea.
+
+Tiene la siguiente estructura:
+
+condicion ? expresion1 : expresion2
+
+Veamos un ejemplo:
+
+edad = 18
+edad >= 18 ? console.log("Eres mayor de edad") : console.log("Eres menor de edad")
+En este código, la variable edad tiene un valor de 18. Luego, se evalúa si la variable edad es mayor o igual a 18. Si es verdadero, 
+se ejecuta console.log("Eres mayor de edad"), de lo contrario, se ejecuta console.log("Eres menor de edad")*/
+
+codigo = "1234"
+/* Escribe tu código aquí */
+
+codigo == "1234" ? console.log("Código correcto") : console.log("Código incorrecto")
+
+
+/* Fin */
+
+/*El if ternario es una forma de escribir un if en una sola línea.*/
